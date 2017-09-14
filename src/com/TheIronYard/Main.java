7879b314c76562f409b43603d339a4cae72f4622
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-09-14 14:21:30 -0500

    fill tests and define some variables

--- src/com/TheIronYard/Main.java
+++ src/com/TheIronYard/Main.java
@@ -1,8 +1,12 @@
+package com.TheIronYard;
+
 /**
  * Test Driven Development (TDD)
  * Write your tests first and watch them fail.
  * Then write the algorithm and watch the tests pass.
  */
+
+
 public class Main {
     public static void main(String[] args) {
         ReversePolishCalc rpc = new ReversePolishCalc();
@@ -12,19 +16,33 @@ public class Main {
         double actualResult = rpc.calculate(rpnExpression);
         checkResult(rpnExpression, expectedResult, actualResult);
 
-        // Write tests for the other operators (-, *, /)
 
-        // Uncomment this test which has many operators
-//        rpnExpression = "5,1,2,+,4,*,+,3,-";
-//        expectedResult = 14.0;
-//        actualResult = rpn.calculate(rpnExpression);
-//        checkResult(rpnExpression, expectedResult, actualResult);
+        rpnExpression = "8.2,2.1,-";
+        expectedResult = 7.3;
+        actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
+
+        rpnExpression = "4.3,9.2,*";
+        expectedResult = 39.56;
+        actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
+
+        rpnExpression = "31.5,5.25,/";
+        expectedResult = 6.0;
+        actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
+
+        rpnExpression = "5,1,2,+,4,*,+,3,-";
+        expectedResult = 14.0;
+        actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
     }
 
     private static void checkResult(String expression, double expected, double actual) {
         if (expected == actual) {
             System.out.println("SUCCESS: " + expression + " is " + expected);
-        } else {
+        }
+        else {
             System.out.println("ERROR: " + expression + " expected " + expected + " actual " + actual);
         }
     }
--- src/com/TheIronYard/ReversePolishCalc.java
+++ src/com/TheIronYard/ReversePolishCalc.java
@@ -1,40 +1,66 @@
+package com.TheIronYard;
 
 public class ReversePolishCalc {
 
-    // You'll need a variable here to keep track of the top of the stack
+    int stackTop = 0;
 
-    // The array of the input string split up
+
     private String[] tokens;
 
-    // The stack
     private String[] stack;
 
     public double calculate(String input) {
 
-        // 1. Use the String split method to split the string into tokens at the commas
-        tokens = ...
+        tokens = input.split(",");
 
-        // 2. Allocate a stack as big as the number of tokens
-        stack = ...
+
+        stack = new String[tokens.length];
 
         // 3. write the algorithm
         for(int i = 0; i < tokens.length; ++i) {
             // calls to push() and pop() and do the math here
+
+            if(isANumber(tokens[i])){
+
+
+
+            }
+
+
         }
 
         // 4. return the result
         return pop();
     }
 
+
+    public static boolean isANumber(String string){
+        try{
+            double stringDouble = Double.parseDouble(string);
+        }catch (NumberFormatException ex){
+            return false;
+        }
+        return true;
+    }
+
+
+
     private void push(String number) {
-        // push on the stack
+
+        stack[stackTop++] = number;
+
     }
 
     private void push(double d) {
-        // change the double to a string and then push it on the stack
+        push(d+"");
     }
 
     private double pop() {
-        // remove the string from the top of the stack and convert it to a double and return it
+
+        double removeString = Double.parseDouble(stack[stackTop--]);
+
+        return removeString;
+
+
     }
 }
